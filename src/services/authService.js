8588862e8947adad16b8cc8437e9038d32819528
Sanika1319 <sanikamulikofficial@gmail.com>
2026-03-17 15:55:39 +0530

    some error solve

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://pacific-unity-production.up.railway.app"; //base url
+const BASE_URL = "https://e-commerce-system-production-b11c.up.railway.app"; //base url
 // const BASE_URL = "http://localhost:8080"; //base url
 
 // object AXIOS
